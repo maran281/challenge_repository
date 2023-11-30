020b374db12822293545efa53a8e3de29de123bb
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-30 22:13:19 +0100

    commit1

---
+++ challenge-1-cicd/terraform/main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+    project = "dev-project-406720"
+    region = "us-east1"
+}
+
+terraform {
+  backend "gcs" {
+
+    bucket = "tf_statebucket_4_challenge-1-cicd"
+    
+  }
+}
+
+resource "google_storage_bucket" "cf_source_code_4_challenge-1-cicd" {
+    
+    name = "cf_source_code_4_challenge"
+    location = "us-east1"
+}
+
+resource "google_storage_bucket_object" "cf_code_object_4_challenge-1-cicd" {
+    name = "main.zip"
+    bucket = google_storage_bucket.cf_source_code_4_challenge-1-cicd.name
+    source = "../code/main.zip"
+}
+
+resource "google_storage_bucket" "cf_source_bucket_4_challenge-1-cicd" {
+    
+    name = "cf_source_bucket_4_challenge-1-cicd"
+    location = "us-east1"
+}
+
+resource "google_storage_bucket" "cf_target_bucket_4_challenge-1-cicd" {
+    
+    name = "cf_target_bucket_4_challenge-1-cicd"
+    location = "us-east1"
+}
+
+resource "google_cloudfunctions_function" "cf_4_challenge-1-cicd" {
+
+    name = "cf_4_challenge-1-cicd"
+    entry_point = "publish_file"
+    runtime = "python310"
+    source_archive_bucket = google_storage_bucket.cf_source_code_4_challenge-1-cicd.name
+    source_archive_object = google_storage_bucket_object.cf_code_object_4_challenge-1-cicd.name
+}
